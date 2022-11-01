87a10daa0248c707696ba016ba11a1df1c84b5c8
Linventif <linventif@gmail.com>
2022-11-01 11:36:16 +0100

    2022 11 01

--- lua/autorun/_linventif_library.lua
+++ lua/autorun/_linventif_library.lua
@@ -1,6 +1,6 @@
 LinvLib = {
     ["name"] = "Linventif Library",
-    ["version"] = "2.0.0",
+    ["version"] = "0.0.6",
     ["author"] = "Linventif",
     ["license"] = "CC BY-SA 4.0",
     ["description"] = "A library for Linventif's scripts."
@@ -38,6 +38,7 @@ function LinvLib.LoadStr(full_name, version, license)
 end
 
 local function LinvLibVerif(LinvLibWeb)
+    LinvLib.LoadStr(LinvLib.name, LinvLib.version, LinvLib.license)
     if LinvLibWeb.version != LinvLib.version then
         print("Linventif Library is outdated! Please update it!")
         print(" ")
@@ -54,13 +55,104 @@ local function LinvLibVerif(LinvLibWeb)
     print(" ")
 end
 
-http.Fetch("https://api.linventif.fr/gmod-lib/info.json",
-	function(body, length, headers, code)
-		LinvLibVerif(util.JSONToTable(body))
-	end,
-	function(message)
-		print(message)
-	end
-)
+hook.Add("Initialize", "LinvLibUpdate", function()
+    timer.Simple( 5, function()
+        http.Fetch("https://api.linventif.fr/gmod-lib/info.json",
+        function(body, length, headers, code)
+            LinvLibVerif(util.JSONToTable(body))
+        end,
+        function(message)
+            print(message)
+        end)
+    end)
+end)
+
+function LinvLib.Loader(folder, name)
+    local files, folders = file.Find(folder .. "/*", "LUA")
+    for k, v in pairs(files) do
+        local path = folder .. "/" .. v
+        local cantLoad = false
+        if string.StartWith(v, "cl_") then
+            print("| " .. name .. " | File Load | " .. path)
+            if SERVER then
+                AddCSLuaFile(path)
+            else
+                include(path)
+            end
+        elseif string.StartWith(v, "sv_") then
+            print("| " .. name .. " | File Load | " .. path)
+            if SERVER then
+                include(path)
+            end
+        elseif string.StartWith(v, "sh_") then
+            print("| " .. name .. " | File Load | " .. path)
+            if SERVER then
+                AddCSLuaFile(path)
+            end
+            include(path)
+        else
+            print("| " .. name .. " | - Error - | File Name Invalid : " .. path)
+        end
+    end
+    for k, v in pairs(folders) do
+        loadFiles(folder .. "/" .. v)
+    end
+end
+
+function LinvLib.ValidLang(folder, lang)
+    if file.Exists(folder .. "/languages/" .. lang .. ".lua", "LUA") then
+        return true
+    else
+        return false
+    end
+end
+
+function LinvLib.ServerName()
+    local f = file.Open("cfg/server.cfg", "r", "MOD" )
+    while true do
+        local line = f:ReadLine()
+        if not line then break end
+        if string.find(line, "hostname") then
+            return line
+        end
+    end
+    f:Close()
+end
+
+function LinvLib.GetValidLang(folder)
+    local serverName = LinvLib.ServerName()
+    if string.find(serverName, "[FR]") && LinvLib.ValidLang(folder, "french") then
+        lang = "french"
+    elseif string.find(serverName, "[EN]") && LinvLib.ValidLang(folder, "english") then
+        lang = "english"
+    elseif string.find(serverName, "[ES]") && LinvLib.ValidLang(folder, "spanish") then
+        lang = "spanish"
+    elseif string.find(serverName, "[DE]") && LinvLib.ValidLang(folder, "german") then
+        lang = "german"
+    elseif string.find(serverName, "[IT]") && LinvLib.ValidLang(folder, "italian") then
+        lang = "italian"
+    elseif string.find(serverName, "[RU]") && LinvLib.ValidLang(folder, "russian") then
+        lang = "russian"
+    elseif string.find(serverName, "[PT]") && LinvLib.ValidLang(folder, "portuguese") then
+        lang = "portuguese"
+    elseif string.find(serverName, "[NL]") && LinvLib.ValidLang(folder, "dutch") then
+        lang = "dutch"
+    elseif string.find(serverName, "[PL]") && LinvLib.ValidLang(folder, "polish") then
+        lang = "polish"
+    elseif string.find(serverName, "[TR]") && LinvLib.ValidLang(folder, "turkish") then
+        lang = "turkish"
+    elseif string.find(serverName, "[CN]") && LinvLib.ValidLang(folder, "chinese") then
+        lang = "chinese"
+    elseif string.find(serverName, "[JP]") && LinvLib.ValidLang(folder, "japanese") then
+        lang = "japanese"
+    elseif string.find(serverName, "[KR]") && LinvLib.ValidLang(folder, "korean") then
+        lang = "korean"
+    elseif string.find(serverName, "[AR]") && LinvLib.ValidLang(folder, "arabic") then
+        lang = "arabic"
+    else
+        lang = "english"
+    end
+    return lang
+end
 
 LinvLib.LoadStr(LinvLib.name, LinvLib.version, LinvLib.license)
