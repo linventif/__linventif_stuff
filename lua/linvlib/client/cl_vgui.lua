c01342bee1d1b432fead332bced085ebe100dd98
Linventif <linventif@gmail.com>
2022-12-20 12:36:56 +0100

    2022 12 20

--- lua/linvlib/client/cl_vgui.lua
+++ lua/linvlib/client/cl_vgui.lua
@@ -24,3 +24,37 @@ function LinvLib.Hover(element, round, border, color, hovercolor, bordercolor, b
         end
     end
 end
+
+function LinvLib.Hover2(element, round, roundborder, border, color, hovercolor, bordercolor, bordercolorhover)
+    local borderx2 = 0
+    if border then
+        borderx2 = border * 2
+    end
+    element.OnCursorEntered = function()
+        element.Paint = function(self, w, h)
+            if border > 0 then
+                draw.RoundedBox(round, 0, 0, w, h, bordercolorhover)
+                draw.RoundedBox(roundborder, border, border, w-borderx2, h-borderx2, hovercolor)
+            else
+                draw.RoundedBox(round, 0, 0, w, h, hovercolor)
+            end
+        end
+    end
+    element.OnCursorExited = function()
+        element.Paint = function(self, w, h)
+            if border > 0 then
+                draw.RoundedBox(round, 0, 0, w, h, bordercolor)
+                draw.RoundedBox(roundborder, border, border, w-borderx2, h-borderx2, color)
+            else
+                draw.RoundedBox(round, 0, 0, w, h, color)
+            end
+        end
+    end
+end
+
+function LinvLib.HideVBar(vbar)
+    vbar:SetWide(0)
+    vbar.btnUp:SetWide(0)
+    vbar.btnDown:SetWide(0)
+    vbar.btnGrip:SetWide(0)
+end
