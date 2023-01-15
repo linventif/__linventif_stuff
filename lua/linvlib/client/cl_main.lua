62dbb66ad93838898417db61ef6549c3bc2ba020
Linventif <linventif@gmail.com>
2023-01-15 04:47:41 +0100

    2023 01 15

--- lua/linvlib/client/cl_main.lua
+++ lua/linvlib/client/cl_main.lua
@@ -21,3 +21,28 @@ function LinvLib.LNotif(msg, enum, time, addon)
     notification.AddLegacy(msg, enums[enum], time)
     print(addon .. " : " .. msg)
 end
+
+function LinvLib.CreateImgurMaterials(materials, addon_var, folder, name)
+    if !file.Exists(folder, "DATA") then
+        file.CreateDir(folder)
+    end
+
+    local function getMatFromUrl(url, id)
+        materials[id] = Material("nil")
+
+        if file.Exists(folder .. "/" .. id .. ".png", "DATA") then
+            addon_var[id] = Material("../data/" .. folder .. "/" .. id .. ".png", "noclamp smooth")
+            return
+        end
+
+        http.Fetch(url, function(body)
+            file.Write(folder .. "/" .. id .. ".png", body)
+            addon_var[id] = Material("../data/" .. folder .. "/" .. id .. ".png", "noclamp smooth")
+            print("| " .. name .. " | Image Downloaded | " .. id .. ".png")
+        end)
+    end
+
+    for k, v in pairs(materials) do
+        getMatFromUrl("https://i.imgur.com/" .. v .. ".png", k)
+    end
+end
--- lua/linvlib/server/sv_main.lua
+++ lua/linvlib/server/sv_main.lua
@@ -1,14 +1,18 @@
-function LinvLib.AddRessource(nam, folder, addon_folder)
-    if SERVER then
-        local files, folders = file.Find(folder.."*", "GAME")
-        for k, v in pairs(folders) do
-            LinvLib.LoadMaterials(folder..v.."/")
-        end
-        for k, v in pairs(files) do
-            resource.AddFile(folder..v)
-            print("| " .. name .. " | Resource Add | " .. addon_folder .. "/"..v)
-        end
+function LinvLib.AddRessource(name, folder, addon_folder)
+    local files, folders = file.Find(folder.."*", "GAME")
+    for k, v in pairs(folders) do
+        LinvLib.LoadMaterials(folder..v.."/")
     end
+    for k, v in pairs(files) do
+        resource.AddSingleFile(folder..v)
+        print("| " .. name .. " | Resource Add | " .. addon_folder .. "/"..v)
+    end
+end
+
+function LinvLib.LoadMaterials(folder, name)
+    local addon_folder = folder
+    folder = "addons/linventif_" .. folder
+    LinvLib.AddRessource(name, folder, addon_folder)
 end
 
 function LinvLib.LoadWorkshop(workshop, name)
